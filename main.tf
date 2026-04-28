e013b05c2342b14fbeb360d39e89941304f79fae
Praveen Cherukuri <praveen_cherukuri@epam.com>
2026-04-29 00:27:34 +0530

    Updated code for EC2 module

---
+++ main.tf
@@ -0,0 +1,140 @@
+/*
+* AWS EC2 Module
+* Managed by Praveen Cherukuri
+*/
+
+##################################################################################################
+###################################### EC2 Instance Module #######################################
+##################################################################################################
+
+# local variable to map OS to AMI filters
+locals {
+  ami_filters = tomap({
+    ubuntu      = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+    amazonlinux = ["al2023-ami-*"]
+    rhel        = ["RHEL-9*"]
+  })
+}
+
+# Data source to get the latest AMI based on the provided filters
+data "aws_ami" "this" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = var.ami_name == "" ? local.ami_filters[var.os] : [var.ami_name]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+#local for tags to be applied to EC2 instance
+locals {
+  # local to create a map of tags to be applied to EC2 instance by merging the default tags with the user provided tags
+  tags = ({
+    os          = var.os
+    application = var.application
+    environment = var.environment
+    project     = var.project
+    backup      = var.backup
+    owner       = var.owner
+    app_owner   = var.app_owner
+    description = var.description
+  })
+
+  # local to az_filter to get the availability zones based on the provided availability_zone variable
+  az_filter = (var.availability_zone == null ? data.aws_availability_zones.this.names : ["${var.region}${var.availability_zone}"])
+
+  # local to determine the instance role based on the provided instance_role variable
+  instance_role = (var.instance_role != null ? var.instance_role : "V-EC2-SSM")
+}
+
+# Data source to get the availability zones based on the provided availability_zone variable
+data "aws_availability_zones" "this" {
+  state = "available"
+}
+
+# Data source to get the VPC details based on the provided vpc_name variable
+data "aws_vpc" "this" {
+  filter {
+    name   = "tag:Name"
+    values = [upper(var.vpc_name)]
+  }
+}
+
+# Data source to get the public subnets in the VPC based on the provided vpc_name variable
+data "aws_subnets" "public" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.this.id]
+  }
+
+  filter {
+    name   = "tag:Name"
+    values = [upper("${data.aws_vpc.this.id}-subnet-public-*")]
+  }
+}
+
+# Data source to get the details of each private subnet
+data "aws_subnet" "public" {
+  for_each = toset(data.aws_subnets.public.ids)
+
+  id = each.value
+}
+
+# Data source to get the private subnets in the VPC based on the provided vpc_name variable
+data "aws_subnets" "private" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.this.id]
+  }
+
+  filter {
+    name   = "tag:Name"
+    values = [upper("${data.aws_vpc.this.id}-subnet-private-*")]
+  }
+}
+
+# Data source to get the details of each private subnet
+data "aws_subnet" "private" {
+  for_each = toset(data.aws_subnets.private.ids)
+
+  id = each.value
+}
+
+resource "aws_instance" "this" {
+  ami                  = data.aws_ami.this.id
+  instance_type        = var.instance_type
+  iam_instance_profile = var.iam_instance_profile
+  # subnet_id            = length(data.aws_subnets.public.ids) > 0 ? data.aws_subnets.public.ids[0] : data.aws_subnets.private.ids[0]
+  subnet_id         = element(concat(data.aws_subnets.public.ids, data.aws_subnets.private.ids), 0)
+  security_groups   = var.security_groups
+  key_name          = var.key_pair
+  availability_zone = local.az_filter[0]
+  private_ip        = var.ip_address
+  user_data = templatefile("${path.root}/scripts/userdata/${var.user_data_template_name}.sh",
+    {
+      environment = var.environment
+    }
+  )
+
+  tags = merge({
+    Name = upper("${var.environment}-${var.project}-${var.application}")
+  }, local.tags, var.tags)
+
+  root_block_device {
+    delete_on_termination = true
+    encrypted             = true
+    volume_type           = "gp3"
+    volume_size           = var.os_disk_size
+
+    tags = merge({
+      Name = upper("${var.environment}-${var.project}-${var.application}-osdisk")
+    }, local.tags, var.tags)
+  }
+
+}
+
---
+++ variables.tf
@@ -0,0 +1,171 @@
+variable "region" {
+  description = "region details"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "tags" {
+  description = "default tags"
+  type        = map(string)
+  default = {
+    "env" = "Dev"
+  }
+}
+
+variable "ami_name" {
+  description = "AMI Name"
+  type        = string
+  default     = "ubuntu"
+}
+
+variable "os" {
+  description = "Operating System"
+  type        = string
+
+  validation {
+    condition     = contains(["ubuntu", "amazonlinux", "rhel"], var.os)
+    error_message = "Invalid OS."
+  }
+}
+
+variable "application" {
+  description = "Application tag value for the EC2 instance. Minimum of 8 characters."
+  type        = string
+
+  validation {
+    condition     = length(var.application) > 7
+    error_message = "application value should be minimum of 8 characters"
+  }
+}
+
+variable "environment" {
+  description = "Environment of the EC2 instance. Possible values: 'Dev','Qa','Stage','PreProd','Production'"
+  type        = string
+  default     = "Dev"
+
+  validation {
+    condition     = contains(["Dev", "Qa", "Stage", "PreProd", "Production"], var.environment)
+    error_message = "Environment should be 'Dev','Qa','Stage','PreProd','Production'"
+  }
+}
+
+variable "project" {
+  description = "Project for which EC2 instance is being created"
+  type        = string
+  default     = ""
+
+  validation {
+    condition     = length(var.project) > 7 || var.project == ""
+    error_message = "Project value should be minimum of 8 characters or empty"
+  }
+}
+
+variable "backup" {
+  description = "Backup to which the EC2 instance belongs. Possible values: 'N/A', 'NonProd', 'Prod'"
+  type        = string
+  default     = "N/A"
+
+  validation {
+    condition     = contains(["N/A", "NonProd", "Prod"], var.backup)
+    error_message = "Backup must be only these values 'N/A', 'NonProd', 'Prod'."
+  }
+}
+
+variable "owner" {
+  description = "Email address of the EC2 instance owner."
+  type        = string
+
+  validation {
+    condition     = can(regex("^[A-Za-z0-9._%+-]+@veen\\.com$", var.owner))
+    error_message = "Owner email must be a valid @veen.com address."
+  }
+}
+
+variable "app_owner" {
+  description = "Email address of the application owner."
+  type        = string
+
+  validation {
+    condition     = can(regex("^[A-Za-z0-9._%+-]+@veen\\.com$", var.app_owner))
+    error_message = "Application owner email must be a valid @veen.com address."
+  }
+}
+
+variable "description" {
+  description = "Description for which EC2 instance is being created"
+  type        = string
+
+  validation {
+    condition     = length(var.description) > 9
+    error_message = "Description value should be minimum of 10 characters"
+  }
+}
+
+variable "availability_zone" {
+  type    = string
+  default = null
+}
+
+variable "vpc_name" {
+  description = "vpc name"
+  type        = string
+  default     = "MANAGEMENT_VPC"
+
+  validation {
+    condition     = contains(["MANAGEMENT_VPC", "DEV_VPC", "STAGE_VPC", "PREPROD_VPC", "PRODUCTION_VPC"], var.vpc_name)
+    error_message = "VPC name should be one of the allowed values."
+  }
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "iam_instance_profile" {
+  description = "IAM instance profile to be attached to the EC2 instance"
+  type        = string
+  default     = ""
+}
+
+variable "instance_role" {
+  description = "Instance role for the EC2 instance"
+  type        = string
+  default     = null
+}
+
+variable "security_groups" {
+  description = "Security Group to be included on the EC2 instance."
+  type        = list(any)
+  default     = []
+}
+
+variable "key_pair" {
+  description = "Keypair to be used on the EC2 instance."
+  type        = string
+  default     = "MANAGED_KEY"
+}
+
+variable "ip_address" {
+  description = "Private IP Address to be supplied"
+  type        = string
+  default     = ""
+
+  validation {
+    condition     = can(regex("^[0-9]{2,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}$", var.ip_address)) || var.ip_address == ""
+    error_message = "Please enter a valid IP address or leave it blank to auto-pick."
+  }
+}
+
+variable "user_data_template_name" {
+  description = "Name of the user data template file (without extension)"
+  type        = string
+  default     = "userdata"
+}
+
+variable "os_disk_size" {
+  description = "Size of the OS disk in GB"
+  type        = number
+  default     = 30
+}
