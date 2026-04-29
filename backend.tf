30e4d6b15351b58fcb8b1af49ac46250eae769e1
Praveen Cherukuri <praveen_cherukuri@epam.com>
2026-04-29 21:03:25 +0530

    EC2 module

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "277802554635-terraform-remote-backend"
-    key            = "DEV/EC2-TEST/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "277802554635-terraform-remote-backend-lock"
-  }
-}
