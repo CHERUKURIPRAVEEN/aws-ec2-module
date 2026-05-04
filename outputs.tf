00898cb2acb98e3327a9dcf33b60139a09067262
Praveen Cherukuri <praveen_cherukuri@epam.com>
2026-05-04 14:41:01 +0530

    added outputs

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,16 @@ output "instance_name" {
   value = aws_instance.this[*].tags["Name"]
 }
 
+output "private_ips" {
+  description = "Private IP addresses of EC2 instances"
+  value       = aws_instance.this[*].private_ip
+}
+
+output "public_ips" {
+  description = "Public IP addresses of EC2 instances"
+  value       = aws_instance.this[*].public_ip
+}
+
 output "public_subnets_found" {
   value = [for s in data.aws_subnet.public : { id = s.id, az = s.availability_zone }]
 }
